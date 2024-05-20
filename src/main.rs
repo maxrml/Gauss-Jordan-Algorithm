1fd65f06a41a79842c622f76a54e7e19d568bdf1
maxrml <max.rml@web.de>
2024-05-20 11:07:36 +0200

    Commenting

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ How can you implement the Gauss-Jordan-Algorithm to bring a matrix in their row 
 
 With this program I want to offer a quite easy way to do so, since there is not the one right algorithm,
 there are many ways to accomplish the Gauss-Elimination. My way might not be the best in terms of time complexity
-but for the sake of general complexity "coding-difficulty-like" this might be a good choice!
+[O(m * n * log(n))] but for the sake of general complexity "coding-difficulty-like" this might be a good choice!
 
 The idea is quite simple and works in a few steps:
 1. The Matrix, represented by a Vector of i32 Vectors, gets sorted such that rows with the more zeros in
@@ -20,31 +20,33 @@ This process goes on till you reach the last line of the matrix!
 
 fn main() {
     // First part: Terminal handling of the input into a matrix
-    let mut matrix: Vec<Vec<i32>> = Vec::new(); //Define matrix
-    let mut input = String::new(); //Create variable for the input
+    let mut matrix: Vec<Vec<i32>> = Vec::new();
+    let mut input = String::new();
     println!("Enter the matrix you want to get in row echelon form!");
     println!("Please give your inputs row by row(just i32), seperated by a semicolon in the following form:");
     println!("1 2 3 ; 2 3 4 ; 3 4 5");
-    std::io::stdin() //Command Input
-        .read_line(&mut input) //Read the command line
-        .expect("Error by reading your Input!"); //Error handling
+    std::io::stdin()
+        .read_line(&mut input)
+        .expect("Error by reading your Input!");
     let unedited_rows: Vec<&str> = input.split(';').collect(); //Split the input by semicolons
     for unedited_row in unedited_rows { //Iterate through the by semicolon split strings
         let edited_row: Vec<i32> = unedited_row //Create an instance for the edited rows, which should include each one i32
             .trim()//Delete whitespace in the end and beginning and line breaks
             .split_whitespace()//Split the remaining numbers by whitespace
             .map(|num| num.parse()//Use for each split i32 the closure num.parse() such that each string number gets an i32 value
-                .expect("Error by parsing your number!"))//Error handling
+                .expect("Error by parsing your number!"))
             .collect();//Safe each i32 in the vec edited_row
-        matrix.push(edited_row);//Add the vec in to the vector of vectors(matrix)
+        matrix.push(edited_row);
     }
     // Second part: From the input matrix to the matrix in row echelon form
-    let solved_matrix = gauss_elimination(&mut matrix); //Use the Gauss method
+    let solved_matrix = gauss_elimination(&mut matrix);
     // Third part: Print the Vec<Vec<i32>> as an actual matrix
-    let mut counter = 0; // Create a counter such that the matrix uses line breaks at the right point
+    let mut counter = 0;
+    // Create a counter such that the matrix uses line breaks at the right point
     let row_length = solved_matrix[0].len();
-    for row in solved_matrix.iter() { //Print Vec<Vec<>> as a Matrix
-        for element in row.iter() { //For each element printed the counter goes up by 1, when the counter equals the row length set it back to 0 and do a line break
+    for row in solved_matrix.iter() {
+        for element in row.iter() {
+            //For each element printed the counter goes up by 1, when the counter equals the row length set it back to 0 and do a line break
             counter += 1;
             if counter == row_length {
                 println!("{element:?}]");
@@ -61,14 +63,18 @@ fn gauss_elimination(matrix: &mut Vec<Vec<i32>>) -> &mut Vec<Vec<i32>> {
     let current_row = 1;
     while current_row < matrix.len() {
         sort(matrix);
+        //Find pivots
         let mut pivot_space = matrix[current_row].iter().position(|&x| x != 0);
 
         match pivot_space {
+            //Check if there is a pivot
             Some(pivot_index) => {
                 let mut n = 0;
+                // Iterate through n rows below and check if there are als pivots on the same space
                     while current_row + n < matrix.len() && matrix[current_row + n].iter().position(|&x| x != 0) == Some(pivot_index) {
                         n += 1;
                     }
+                // Eliminate the pivot elements on the n rows below
                 while n != 0 {
                     let new_row = row_add(&mut matrix[current_row].clone(), &mut matrix[current_row+n-1].clone(), pivot_space.unwrap());
                     let _ = std::mem::replace(&mut matrix[current_row + n - 1], new_row);
@@ -83,13 +89,14 @@ fn gauss_elimination(matrix: &mut Vec<Vec<i32>>) -> &mut Vec<Vec<i32>> {
 }
 
 fn sort(matrix: &mut Vec<Vec<i32>>) -> &mut Vec<Vec<i32>>{
-
+    //Sort the rows such that rows with many zeros in the front are down
     matrix.sort_by_key(|vec| count_leading_zeros(vec));
 
     matrix
 }
 
 fn count_leading_zeros(vec: &Vec<i32>) -> usize {
+    //Function needed as an argument for the sort_by_key
     vec.iter()
         .take_while(|&&x| x==0)
         .count()
